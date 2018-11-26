c121edc8c0c0a57501b46dcc327b94b8de998616
seintun <seintun.edu@gmail.com>
2018-11-26 14:34:11 -0800

    Added migrations and dummy seeds for toppings table

---
+++ src/db/migrations/03_toppings_migration.js
@@ -0,0 +1,13 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('toppings', (table) =>{
+    table.increments();
+    table.string('name').notNullable().defaultTo('');
+    table.string('description', 500).defaultTo('');
+    table.integer('calorie').defaultTo(0);
+    table.timestamps(true,true);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('toppings');
+};
